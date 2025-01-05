11a3ba0c323d42417eb3eccc506b70241ea27ab1
AtharvPorate1 <www.atharvporate254@gmail.com>
2025-01-06 00:59:27 +0530

    bg-color update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -261,7 +261,7 @@ export default function Component() {
   }
 
   return (
-    <div className="min-h-screen bg-darkbg text-white p-4 md:p-8">
+    <div className="min-h-screen bg-[#0c0c0c] text-white p-4 md:p-8">
       <div className="max-w-[90%] md:max-w-[70%] mx-auto">
         {/* <Link href="#" className="inline-flex w-full justify-center my-10 items-center text-2xl font-bold text-ow1 transition-colors">
           <span className="hover:underline">[ Go Back ]</span>
