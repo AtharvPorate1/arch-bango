d750b7f911ddad67f1f61c7a81d6b9e4c5c1a3f5
AtharvPorate1 <www.atharvporate254@gmail.com>
2025-02-26 22:10:36 +0530

    hero update

--- components/_live/hero.tsx
+++ components/_live/hero.tsx
@@ -1,13 +1,16 @@
+import { Inter } from "next/font/google"
+
+// const inter = Inter({ subsets: ["latin"] })
+
 export default function HeroSection() {
-    return (
-        <div className="w-full max-w-6xl mx-auto px-4 py-8 dm-sans">
-        <div className="flex items-center justify-center min-h-[208px] bg-[#111111] rounded-lg">
-          <h1 className="text-xl md:text-[18px] font-bold dm-sans text-center text-transparent bg-clip-text bg-gradient-to-r from-[#FF5733] to-[#FFA07A] px-4">
-            AI powered Prediction Market on Bitcon
-          </h1>
-        </div>
+  return (
+    <div className={`w-full max-w-6xl mx-auto px-4 py-8 dm-sans`}>
+      <div className="flex items-center justify-center min-h-[208px] rounded-lg bg-[#EC762E] ">
+        <h1 className="text-2xl md:text-4xl font-bold text-center text-white px-4">
+          Predict the Future, Invest in Possibilities
+        </h1>
       </div>
-    )
-  }
-  
-  
+    </div>
+  )
+}
+
